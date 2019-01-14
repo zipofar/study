192086a910ab1169e49a70aea11dab06497083aa
Zipofar <admin@zipofar.ru>
2019-01-14 22:43:10 +0300

    Refactor functions at fp test

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,31 +5,17 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        i = 0
-
-        sum_of_rating = array.reduce(0) do |memo, film|
-          rating = film['rating_kinopoisk'].to_f
-          country = film['country']
-          count_countries = country.nil? ? 0 : country.split(',').length
-
-          if rating > 0 && count_countries > 1
-            memo += rating
-            i += 1
-          end
-          memo
-        end
-        sum_of_rating / i
+        filteredFilms = array.select { |film|
+          film['country'].nil? ? false : film['country'].split(',').length >= 2 }
+            .select { |film| film['rating_kinopoisk'].to_f > 0  }
+        sum_of_rating = filteredFilms.reduce(0) { |memo, film| memo += film['rating_kinopoisk'].to_f }
+        sum_of_rating / filteredFilms.length
       end
 
       def chars_count(films, threshold)
-        count_chars = 0
-
-        films.map do |film|
-          if film['rating_kinopoisk'].to_f >= threshold
-            count_chars += film['name'].chars.reduce(0) { |a, e| e == 'и' ? a += 1 : a }
-          end
-        end
-        count_chars
+        count = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+          .map { |film| film['name'].chars.reduce(0) { |acc, e| e == 'и' ? acc += 1 : acc } }
+          .reduce(0) { |acc, e| acc += e }
       end
     end
   end
